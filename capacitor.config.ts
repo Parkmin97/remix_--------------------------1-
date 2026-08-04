abedaf0b8faf14875d19450bfcdb44ec8c53380c
Parkmin97 <p2h2p97@gmail.com>
2026-08-04 18:58:52 +0900

    안드로이드 앱개발 시작

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mylifemaestro.app',
+  appName: '내인생 지휘자',
+  webDir: 'dist'
+};
+
+export default config;
